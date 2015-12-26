5fe03ad75e4b678301f87b4886eb8ae0da25471a
Hangbokie <sungkisa@naver.com>
2015-12-26 09:55:57 +0900

    new site

---
+++ resources/views/index.blade.php
@@ -0,0 +1,115 @@
+<!DOCTYPE html>
+<html lang="ko">
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<title>Jeju Project</title>
+		<link rel="stylesheet" href="./css/font-awesome.min.css">
+		<link href="//fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+		<link rel="stylesheet" href="./css/materialize.min.css">
+		<link rel="stylesheet" href="./css/grid.min.css">
+		<link rel="stylesheet" href="./css/style.min.css">
+		<script src="./js/jquery.min.js"></script>
+	</head>
+	<body class="blue-grey lighten-4">
+		<div class="navbar-fixed">
+			<nav class="blue-grey">
+				<div class="container">
+					<div class="nav-wrapper">
+						<a href="#" class="brand-logo center"><img src="./image/logo.png" class="nav-logo"></a>
+						<a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>				
+						<ul id="nav-mobile" class="left hide-on-med-and-down">
+							<li><a href="#">Menu1</a></li>
+							<li><a href="#">Menu2</a></li>
+							<li><a href="#">Menu3</a></li>
+							<li><a href="#">Menu4</a></li>
+						</ul>
+						<ul class="side-nav" id="mobile-demo">
+							<li><a href="#">Menu1</a></li>
+							<li><a href="#">Menu2</a></li>
+							<li><a href="#">Menu3</a></li>
+							<li><a href="#">Menu4</a></li>
+						</ul>
+					</div>
+				</nav>
+			</div>
+		</div>
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12">
+					<div class="card">
+						<span class="card-header">Jeju Engine is Powerful Engine for Android Devices.</span><hr>
+					제주엔진은 안드로이드 폰을 위한 강력한 엔진입니다.
+					</div>
+				</div>
+				<div class="col-md-4">
+					<div class="card">
+						<span class="card-header"><span class="fa fa-github-square"></span> Github</span><hr>
+						<a class="truncate" href="https://github.com/jejuproject/Engine">https://github.com/jejuproject/Engine</a>
+					</div>
+				</div>
+				<div class="col-md-4">
+					<div class="card">
+						<span class="card-header"><span class="fa fa-users"></span> Developers</span><hr>
+						<ul class="collection">
+							<li class="collection-item avatar">
+								<img src="https://avatars2.githubusercontent.com/u/9509286?v=3&s=400" class="circle">
+								<span class="title">Hangbokie</span>
+								<p>Leader<br><a href="https://github.com/hangbokie"><span class="fa fa-github-square"></span> Github</span></a></p>
+							</li>
+							<li class="collection-item avatar">
+								<img src="https://avatars0.githubusercontent.com/u/14028505?v=3&s=400" class="circle">
+								<span class="title">leemchaehoon</span>
+								<p>Coder<br><a href="https://github.com/leemchaehoon"><span class="fa fa-github-square"></span> Github</span></a></p>
+							</li>
+							<li class="collection-item avatar">
+								<img src="https://avatars2.githubusercontent.com/u/9381622?v=3&s=400" class="circle">
+								<span class="title">lq092009</span>
+								<p>Coder<br><a href="https://github.com/lq092009"><span class="fa fa-github-square"></span> Github</span></a></p>
+							</li>
+							<li class="collection-item avatar">
+								<img src="https://avatars0.githubusercontent.com/u/13872518?v=3&s=400" class="circle">
+								<span class="title">ADRINER</span>
+								<p>Web Developer<br><a href="https://github.com/adriner2640"><span class="fa fa-github-square"></span> Github</span></a></p>
+							</li>
+						</ul>
+					</div>
+				</div>
+				<div class="col-md-4">
+					<div class="card">
+						<span class="card-header"><span class="fa fa-cloud-download"></span> Download</span><hr>
+						Jeju Engine: <a class="btn disabled btn-flat">다운로드</a>
+					</div>
+				</div>
+			</div>
+		</div>
+		<footer class="page-footer blue-grey">
+			<div class="container">
+				<div class="row">
+					<div class="col l6 s12">
+						<h5 class="white-text">Footer Content</h5>
+						<p class="grey-text text-lighten-4">You can use rows and columns here to organize your footer content.</p>
+					</div>
+					<div class="col l4 offset-l2 s12">
+						<h5 class="white-text">Links</h5>
+						<ul>
+							<li><a class="grey-text text-lighten-3" href="#!">Link 1</a></li>
+							<li><a class="grey-text text-lighten-3" href="#!">Link 2</a></li>
+							<li><a class="grey-text text-lighten-3" href="#!">Link 3</a></li>
+							<li><a class="grey-text text-lighten-3" href="#!">Link 4</a></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+			<div class="footer-copyright">
+				<div class="container">
+					© 2014 Copyright Text
+					<a class="grey-text text-lighten-4 right" href="#!">More Links</a>
+				</div>
+			</div>
+		</footer>
+		<script src="./js/materialize.min.js"></script>
+		<script src="./js/init.min.js"></script>
+	</body>
+</html>	
